7e8b7a327506e15e7544a3a829cb1bdf7102270d
adamborbely <55229806+adamborbely@users.noreply.github.com>
2020-01-10 15:54:00 +0100

    Hello_world arguments

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Hello World!");
+            }
+            else
+            {
+                bool first = true;
+                foreach (string element in args)
+                {
+                    if (first)
+                    {
+                        first = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Hello " + element + "!");
+                    }
+                }
+
+            }
+        }
+    }
+}
